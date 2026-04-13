cd08a4a1b7cecc0dda3c75df4fa6a07445808487
440791217 <440791217@qq.com>
2026-04-13 16:16:36 +0800

    1
    1

--- Datasets/ImageNet.py
+++
@@ -1,5 +0,0 @@
-import torchvision
-import torch
-
-imagenet_data = torchvision.datasets.ImageNet('data/imagenet/')
-val_dataset = torchvision.datasets.ImageFolder("data/imagenet/val", transform=...)
